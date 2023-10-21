dea7f5b049a7a99735463f77c6c96b4334bc4900
David <hey@davidmuir.co>
2023-10-21 20:43:45 +0100

    Complete parsing of bencode

--- src/bencode.rs
+++ src/bencode.rs
@@ -2,58 +2,74 @@ use anyhow::{anyhow, Result};
 use std::{collections::HashMap, fmt::Display};
 
 pub fn decode_bencoded(input: &str) -> Result<Vec<Decoded>> {
+    let (decodeds, _) = parse_internal(input)?;
+
+    Ok(decodeds)
+}
+
+fn parse_string(input: &str) -> Result<(&str, usize)> {
+    let colon_index = input.find(':').ok_or(anyhow!("Couldn't parse string"))?;
+    let number: usize = input[0..colon_index].parse()?;
+    let end_index = colon_index + 1 + number;
+    let string = &input[colon_index + 1..end_index];
+
+    Ok((string, end_index))
+}
+
+fn parse_integer(input: &str) -> Result<(i64, usize)> {
+    let end_index = input
+        .find('e')
+        .ok_or(anyhow!("Couldn't parse integer - missing end (e)"))?;
+
+    let number = input[1..end_index].parse()?;
+
+    Ok((number, end_index + 1))
+}
+
+fn parse_internal(input: &str) -> Result<(Vec<Decoded>, usize)> {
     let mut current_index = 0;
     let mut decodeds = vec![];
 
     while current_index < input.len() {
         if let Some(current_value) = input.chars().nth(current_index) {
             if current_value.is_ascii_digit() {
-                let colon_index = input[current_index..].find(':').ok_or(anyhow!(
-                    "Couldn't parse string start at {current_index} - missing colon"
-                ))?;
-                let number: usize = input[current_index..colon_index].parse()?;
-                let end_index = colon_index + 1 + number;
-                let string = &input[colon_index + 1..end_index];
+                let (string, end_index) = parse_string(&input[current_index..])?;
 
                 decodeds.push(Decoded::String(string));
-                current_index = end_index;
+                current_index += end_index;
             } else if current_value == 'i' {
-                let end_index = input[current_index..].find('e').ok_or(anyhow!(
-                    "Couldn't parse integer start at {current_index} - missing end (e)"
-                ))?;
-
-                let number = input[current_index..][1..end_index].parse()?;
+                let (number, end_index) = parse_integer(&input[current_index..])?;
 
                 decodeds.push(Decoded::Integer(number));
-                current_index += end_index + 1;
+                current_index += end_index;
             } else if current_value == 'l' {
-                let mut count_e = 1;
-                let mut i = current_index + 1;
+                let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
 
-                while count_e > 0 {
-                    let c = input
-                        .chars()
-                        .nth(i)
-                        .ok_or(anyhow!("Couldn't find closing tag for list"))?;
-                    match c {
-                        'i' | 'l' | 'd' => {
-                            count_e += 1;
-                        }
-                        'e' => {
-                            count_e -= 1;
-                        }
-                        _ => {}
+                decodeds.push(Decoded::List(inner));
+                current_index += end_index;
+            } else if current_value == 'd' {
+                let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
+
+                let mut key = None;
+                let mut dict = HashMap::new();
+
+                for item in inner {
+                    if let Some(k) = key {
+                        let k = match k {
+                            Decoded::String(s) => Ok(s),
+                            _ => Err(anyhow!("Unexpected type for key in dictionary")),
+                        }?;
+                        dict.insert(k, item);
+                        key = None;
+                    } else {
+                        key = Some(item);
                     }
-                    i += 1;
                 }
 
-                let inner_str = &input[(current_index + 1)..(i - 1)];
-
-                decodeds.push(Decoded::List(decode_bencoded(inner_str)?));
-
-                current_index = i + 1;
-            } else if current_value == 'd' {
-                todo!()
+                decodeds.push(Decoded::Dictionary(dict));
+                current_index += end_index;
+            } else if current_value == 'e' {
+                break;
             } else {
                 return Err(anyhow!(
                     "Unexpected value \"{current_value}\" at {current_index}"
@@ -62,7 +78,7 @@ pub fn decode_bencoded(input: &str) -> Result<Vec<Decoded>> {
         }
     }
 
-    Ok(decodeds)
+    Ok((decodeds, current_index + 1))
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -121,6 +137,14 @@ mod tests {
     }
 
     #[test]
+    fn parses_multiple_values() {
+        let input = "5:helloi52e";
+        let expected = vec![Decoded::String("hello"), Decoded::Integer(52)];
+
+        assert_eq!(expected, decode_bencoded(input).unwrap());
+    }
+
+    #[test]
     fn parses_negative_integer() {
         let input = "i-52e";
         let expected = vec![Decoded::Integer(-52)];
@@ -130,10 +154,11 @@ mod tests {
 
     #[test]
     fn parses_list() {
-        let input = "l4:speni7ee";
+        let input = "l4:speni7ed4:spami7eee";
         let expected = vec![Decoded::List(vec![
             Decoded::String("spen"),
             Decoded::Integer(7),
+            Decoded::Dictionary(HashMap::from([("spam", Decoded::Integer(7))])),
         ])];
 
         assert_eq!(expected, decode_bencoded(input).unwrap());
@@ -141,17 +166,6 @@ mod tests {
 
     #[test]
     fn parses_dict() {
-        let input = "d4:spami7ee";
-        let expected = vec![Decoded::Dictionary(HashMap::from([(
-            "spam",
-            Decoded::Integer(7),
-        )]))];
-
-        assert_eq!(expected, decode_bencoded(input).unwrap());
-    }
-
-    #[test]
-    fn parses_nested_dict() {
         let input = "d4:spami7e3:keyd4:spami7eee";
         let expected = vec![Decoded::Dictionary(HashMap::from([
             ("spam", Decoded::Integer(7)),
