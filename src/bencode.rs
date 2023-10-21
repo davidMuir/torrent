812488f875e3c7329c1cf86b6027ac953fba22e5
David <hey@davidmuir.co>
2023-10-21 21:57:46 +0100

    Adjust to use [u8] so as to handle byte strings

--- src/bencode.rs
+++ src/bencode.rs
@@ -1,80 +1,84 @@
 use anyhow::{anyhow, Result};
-use std::{collections::HashMap, fmt::Display};
+use std::{collections::HashMap, fmt::Display, str};
 
-pub fn decode_bencoded(input: &str) -> Result<Vec<Decoded>> {
+pub fn decode_bencoded(input: &[u8]) -> Result<Vec<Decoded>> {
     let (decodeds, _) = parse_internal(input)?;
 
     Ok(decodeds)
 }
 
-fn parse_string(input: &str) -> Result<(&str, usize)> {
-    let colon_index = input.find(':').ok_or(anyhow!("Couldn't parse string"))?;
-    let number: usize = input[0..colon_index].parse()?;
+fn parse_string(input: &[u8]) -> Result<(&[u8], usize)> {
+    let colon_index = input
+        .iter()
+        .position(|v| v == &b':')
+        .ok_or(anyhow!("Couldn't parse string"))?;
+    let number: usize = str::from_utf8(&input[0..colon_index])?.parse()?;
     let end_index = colon_index + 1 + number;
     let string = &input[colon_index + 1..end_index];
 
     Ok((string, end_index))
 }
 
-fn parse_integer(input: &str) -> Result<(i64, usize)> {
+fn parse_integer(input: &[u8]) -> Result<(i64, usize)> {
     let end_index = input
-        .find('e')
+        .iter()
+        .position(|v| v == &b'e')
         .ok_or(anyhow!("Couldn't parse integer - missing end (e)"))?;
 
-    let number = input[1..end_index].parse()?;
+    let number = str::from_utf8(&input[1..end_index])?.parse()?;
 
     Ok((number, end_index + 1))
 }
 
-fn parse_internal(input: &str) -> Result<(Vec<Decoded>, usize)> {
+fn parse_internal(input: &[u8]) -> Result<(Vec<Decoded>, usize)> {
     let mut current_index = 0;
     let mut decodeds = vec![];
 
     while current_index < input.len() {
-        if let Some(current_value) = input.chars().nth(current_index) {
-            if current_value.is_ascii_digit() {
-                let (string, end_index) = parse_string(&input[current_index..])?;
+        let current_value = input[current_index];
 
-                decodeds.push(Decoded::String(string));
-                current_index += end_index;
-            } else if current_value == 'i' {
-                let (number, end_index) = parse_integer(&input[current_index..])?;
+        if current_value.is_ascii_digit() {
+            let (string, end_index) = parse_string(&input[current_index..])?;
 
-                decodeds.push(Decoded::Integer(number));
-                current_index += end_index;
-            } else if current_value == 'l' {
-                let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
+            decodeds.push(Decoded::ByteString(string));
+            current_index += end_index;
+        } else if current_value == b'i' {
+            let (number, end_index) = parse_integer(&input[current_index..])?;
 
-                decodeds.push(Decoded::List(inner));
-                current_index += end_index;
-            } else if current_value == 'd' {
-                let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
+            decodeds.push(Decoded::Integer(number));
+            current_index += end_index;
+        } else if current_value == b'l' {
+            let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
 
-                let mut key = None;
-                let mut dict = HashMap::new();
+            decodeds.push(Decoded::List(inner));
+            current_index += end_index;
+        } else if current_value == b'd' {
+            let (inner, end_index) = parse_internal(&input[(current_index + 1)..])?;
 
-                for item in inner {
-                    if let Some(k) = key {
-                        let k = match k {
-                            Decoded::String(s) => Ok(s),
-                            _ => Err(anyhow!("Unexpected type for key in dictionary")),
-                        }?;
-                        dict.insert(k, item);
-                        key = None;
-                    } else {
-                        key = Some(item);
-                    }
+            let mut key = None;
+            let mut dict = HashMap::new();
+
+            for item in inner {
+                if let Some(k) = key {
+                    let k = match k {
+                        Decoded::ByteString(s) => Ok(s),
+                        _ => Err(anyhow!("Unexpected type for key in dictionary")),
+                    }?;
+                    dict.insert(str::from_utf8(k)?, item);
+                    key = None;
+                } else {
+                    key = Some(item);
                 }
-
-                decodeds.push(Decoded::Dictionary(dict));
-                current_index += end_index;
-            } else if current_value == 'e' {
-                break;
-            } else {
-                return Err(anyhow!(
-                    "Unexpected value \"{current_value}\" at {current_index}"
-                ));
             }
+
+            decodeds.push(Decoded::Dictionary(dict));
+            current_index += end_index;
+        } else if current_value == b'e' {
+            break;
+        } else {
+            return Err(anyhow!(
+                "Unexpected value \"{current_value}\" at {current_index}"
+            ));
         }
     }
 
@@ -83,7 +87,7 @@ fn parse_internal(input: &str) -> Result<(Vec<Decoded>, usize)> {
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum Decoded<'a> {
-    String(&'a str),
+    ByteString(&'a [u8]),
     Integer(i64),
     List(Vec<Decoded<'a>>),
     Dictionary(HashMap<&'a str, Decoded<'a>>),
@@ -92,7 +96,7 @@ pub enum Decoded<'a> {
 impl<'a> Display for Decoded<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Decoded::String(s) => write!(f, "\"{s}\""),
+            Decoded::ByteString(s) => write!(f, "\"{s:?}\""),
             Decoded::Integer(i) => write!(f, "{i}"),
             Decoded::List(l) => {
                 write!(f, "[")?;
@@ -123,9 +127,9 @@ mod tests {
     #[test]
     fn parses_string() {
         let input = "5:hello";
-        let expected = vec![Decoded::String("hello")];
+        let expected = vec![Decoded::ByteString(b"hello")];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 
     #[test]
@@ -133,15 +137,15 @@ mod tests {
         let input = "i52e";
         let expected = vec![Decoded::Integer(52)];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 
     #[test]
     fn parses_multiple_values() {
         let input = "5:helloi52e";
-        let expected = vec![Decoded::String("hello"), Decoded::Integer(52)];
+        let expected = vec![Decoded::ByteString(b"hello"), Decoded::Integer(52)];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 
     #[test]
@@ -149,19 +153,19 @@ mod tests {
         let input = "i-52e";
         let expected = vec![Decoded::Integer(-52)];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 
     #[test]
     fn parses_list() {
         let input = "l4:speni7ed4:spami7eee";
         let expected = vec![Decoded::List(vec![
-            Decoded::String("spen"),
+            Decoded::ByteString(b"spen"),
             Decoded::Integer(7),
             Decoded::Dictionary(HashMap::from([("spam", Decoded::Integer(7))])),
         ])];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 
     #[test]
@@ -175,6 +179,6 @@ mod tests {
             ),
         ]))];
 
-        assert_eq!(expected, decode_bencoded(input).unwrap());
+        assert_eq!(expected, decode_bencoded(input.as_bytes()).unwrap());
     }
 }
